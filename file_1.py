5ed56fe5a9604e25c9b2e45571539ab664998631
Umarfaruk13 <162983011+Umarfaruk13@users.noreply.github.com>
2024-03-11 16:00:48 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,89 @@
+print("HELLO WORLD")
+print("UMAR 209")
+'''
+print("HELLO WORLD")
+print("UMAR 209")
+print("HELLO WORLD")
+print("UMAR 209")
+'''
+print("HELLO WORLD")
+print("UMAR 209")
+
+
+'''
+1.Commenting is used for making documentation of a python file.
+2.when the line is commented,that line is not considered as a part of the code.
+3.when the program ios executed ,commented line will not be in the part of execution.
+
+
+----->NOTE:
+    single line commenting can be done using # at the beginning of the line
+    Eg:
+        print("HELLO WORLD")
+Multiple line commenting can be done using """ code """.capitalize.the lines between
+'''
+
+
+
+import this
+a=89
+b=50
+print(a)
+print(b)
+ ----->Bargained Characters
+ controls and data structures-->ABC language
+ Strings and its formats-->from C
+ Regular expression-->Peart
+ Exception handling-->Module3
+ Magic methods from--->fortron
+
+--> what kind of language is python
+python is a dynamically language
+--> Flavours of python
+  Cpython-written in C, Most common implementation of python
+  Python-Java implementation of python.
+  IronPython-implementation of thr python programming language which is tightly integrated with. NET.
+  Brython-Browser python, runs in browsers
+  Rubypython-Bridge between python and ruby inter preters
+  Micropython
+
+  #--->print(obiect,sep,end,file,flush)
+  #print has 5 parameters
+  #object --->the valuevto be printed is object
+  #sep-->seperator
+  #Eg
+  #a=8
+  #b=9
+  #c=89
+  #print(a,b,c, sep='$')
+  #end -->which is used to add the string at the end of the value,
+  ##it will not keep the command to stay in same line
+  #Eg:
+  #print(str(a), b, end='%')
+  #print(str(c))
+
+
+  #!--->Variables
+  #?variable is a name which is used to identify the value of the variable
+  #?which is stored in a specific memory location
+
+'''
+  #! Rules to define variables
+  # 1.Variable ahve to start with only alphabets,underscores
+  #2.the char following the 1st char can be alphabets,numbers,underscores
+  #3.no spaces are allowed
+  #4.No special characters like %#!@%%^()!
+  #5.case sensitive
+  #6.
+  '''
+
+'''
+#! ways to define variables
+#a=5
+#b=7
+#print(a,b)
+
+
+
+
+ 
